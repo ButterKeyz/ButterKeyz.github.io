b6032d523eca8a5c8ccc03881b4f00a4511f58a2
Bea Cause <spamlyBea@gmail.com>
2024-04-03 10:20:10 -0600

    still fixing mobile...

--- Pages/Demo Link Card (Jadetonic)/script.js
+++ Pages/Demo Link Card (Jadetonic)/script.js
@@ -1,31 +1,30 @@
 //***delay open link... so see animation lol***
-function delay(URL) {
-  setTimeout(function () {
-    window.location = URL;
-  }, 500);
+	
+var icon = document.querySelectorAll(".icon");
+
+function link(el) {
+	var ev =	el.addEventListener("click", function (e) {
+			e.preventDefault();
+  		this.style.animation = "button-push 2s ease 0.01s";
+			setTimeout(function () {
+    		window.location = el.getAttribute("href")
+  		}, 500);
+		}
+	)
 };
 
-var icon = document.querySelector(".icon");
-// icon.onmouseover = function(){
-// 	this.style.animation = "button-hover 0.5s ease 0.01s";
-// }
-// icon.onmouseout = function(){
-// 	this.style.animation = "button-hover 0.5s ease 0.01s 1 reverse";
-// }
-icon.onclick = function(){
-	this.style.animation = "button-push 2s ease 0.01s";
-}
+icon.forEach(link);
 
 //loading screen remove
-//window.addEventListener("load", 
-window.addEventListener("load", () => {
-  var loader = document.querySelector(".loader");
+//window.addEventListener("load"
+document.addEventListener("DOMContentLoaded", () => {
+	var loader = document.querySelector(".loader");
   setTimeout(function () {
     loader.classList.add("loader-hidden");
   loader.addEventListener("transitionend", () => {
 	    loader.remove();
 		});
-  }, 500); 	
+  }, 250); 	
 });
 
 //***blur background on card hover***
